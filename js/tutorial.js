f7907f376b9ffab4ab6a45736ec740cc0c0c2604
Ivan Morozov <morozov.medo@gmail.com>
2025-02-10 18:29:03 +0500

    fix

--- js/tutorial.js
+++ js/tutorial.js
@@ -13,13 +13,11 @@ function createTutorialShape(shape, shapeOffsets, cellSize) {
     dragImage.style.gridTemplateColumns = "repeat(" + maxCol + ", " + cellSize + "px)";
     dragImage.style.width = Math.ceil(cellSize * maxCol) + "px";
     dragImage.style.height = Math.ceil(cellSize * maxRow) + "px";
-
     var blocks = dragImage.querySelectorAll(".block");
     for (var i = 0; i < blocks.length; i++) {
         var block = blocks[i];
         block.style.width = cellSize + "px";
         block.style.height = cellSize + "px";
-
         var crystal = block.querySelector(".crystal");
         if (crystal) {
             crystal.style.width = (cellSize * 0.5) + "px";
@@ -32,89 +30,59 @@ function createTutorialShape(shape, shapeOffsets, cellSize) {
 function startTutorialAnimation() {
     var shape = shapesContainer.querySelector(".shape");
     if (!shape) return;
-
     var finger = document.getElementById("tutorialFinger");
     if (!finger) {
-        finger = document.createElement("img");
-        finger.src = "../images/finger.png";
+        finger = document.createElement("div");
         finger.id = "tutorialFinger";
-        finger.style.position = "absolute";
-        finger.style.pointerEvents = "none";
-        finger.style.zIndex = "9999";
         document.body.appendChild(finger);
     }
-
     function animateCycle() {
         var scaleFactor = getScaleFactor();
         var shapeRect = shape.getBoundingClientRect();
         var startX = shapeRect.left + shapeRect.width / 2;
         var startY = shapeRect.top + shapeRect.height / 2;
-
         var targetCell = cells[59];
         var targetRect = targetCell.getBoundingClientRect();
         var endX = targetRect.left;
         var endY = targetRect.top;
-
         finger.style.transition = "none";
         finger.style.left = startX + "px";
         finger.style.top = startY + "px";
         finger.style.opacity = "1";
         finger.style.transformOrigin = "center center";
-        finger.style.transform =
-            "translate(-50%, -50%) translate(" +
-            (40 * scaleFactor) + "px, " + (50 * scaleFactor) +
-            "px) scale(" + scaleFactor + ")";
-
+        finger.style.transform = "translate(-50%, -50%) translate(" + (45 * scaleFactor) + "px, " + (55 * scaleFactor) + "px) scale(" + scaleFactor + ")";
         void finger.offsetWidth;
-
         var cellSize = playingField.querySelector(".cell").offsetWidth;
         var blocks = shape.querySelectorAll(".block");
         var shapeOffsets = [];
-
         for (var i = 0; i < blocks.length; i++) {
             var block = blocks[i];
             var row = parseInt(block.style.gridRowStart, 10) - 1;
             var col = parseInt(block.style.gridColumnStart, 10) - 1;
             shapeOffsets.push({ row: row, col: col });
         }
-
         var scaledClone = createTutorialShape(shape, shapeOffsets, cellSize);
         scaledClone.id = "scaledClone";
         scaledClone.style.opacity = "0.5";
         scaledClone.style.transformOrigin = "center center";
         scaledClone.style.left = startX + "px";
         scaledClone.style.top = startY + "px";
-        scaledClone.style.transform =
-            "translate(-50%, -50%) scale(" + scaleFactor + ")";
-
+        scaledClone.style.transform = "translate(-50%, -50%) scale(" + scaleFactor + ")";
         document.body.appendChild(scaledClone);
-
         void scaledClone.offsetWidth;
-
         var deltaX = endX - startX;
         var deltaY = endY - startY;
         var transitionStyle = "transform 1s ease-out, opacity 1s ease-out";
-
         finger.style.transition = transitionStyle;
         scaledClone.style.transition = transitionStyle;
-
-        finger.style.transform =
-            "translate(" + deltaX + "px, " + deltaY +
-            "px) translate(-50%, -50%) translate(" +
-            (40 * scaleFactor) + "px, " + (50 * scaleFactor) +
-            "px) scale(" + scaleFactor + ")";
-
-        scaledClone.style.transform =
-            "translate(" + deltaX + "px, " + deltaY +
-            "px) translate(-50%, -50%) scale(" + scaleFactor + ")";
-
+        finger.style.transform = "translate(" + deltaX + "px, " + deltaY + "px) translate(-50%, -50%) translate(" + (45 * scaleFactor) + "px, " + (55 * scaleFactor) + "px) scale(" + scaleFactor + ")";
+        scaledClone.style.transform = "translate(" + deltaX + "px, " + deltaY + "px) translate(-50%, -50%) scale(" + scaleFactor + ")";
         setTimeout(function() {
             if (scaledClone.parentNode) {
                 scaledClone.parentNode.removeChild(scaledClone);
             }
             finger.style.opacity = "0";
         }, 1000);
-
         function onFade(e) {
             if (e.propertyName === "opacity") {
                 finger.removeEventListener("transitionend", onFade);
@@ -123,14 +91,12 @@ function startTutorialAnimation() {
         }
         finger.addEventListener("transitionend", onFade);
     }
-
     animateCycle();
 }
 
 function stopTutorialAnimation() {
     var finger = document.getElementById("tutorialFinger");
     var scaledClone = document.getElementById("scaledClone");
-
     if (finger && finger.parentNode) {
         finger.parentNode.removeChild(finger);
     }
